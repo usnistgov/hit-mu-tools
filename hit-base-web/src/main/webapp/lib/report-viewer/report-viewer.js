1ca4d532da3e9b54b304a30b803cc8e09ca6354c
Affo, Harold <affoharold@yahoo.fr>
2016-03-31 23:49:43 -0400

    added test case execution result and comments feature

--- hit-base-web/src/main/webapp/lib/report-viewer/report-viewer.js
+++ hit-base-web/src/main/webapp/lib/report-viewer/report-viewer.js
@@ -11,7 +11,7 @@
                 restrict: 'A',
                 scope: {
                     type: '@'
-                 },
+                },
                 templateUrl: 'ReportViewer.html',
                 replace: false,
                 controller: 'ReportViewerCtrl'
@@ -20,34 +20,119 @@
     ]);
 
     mod
-        .controller('ReportViewerCtrl', ['$scope', '$rootScope', '$compile', 'ReportService', function ($scope, $rootScope, $compile, ReportService) {
+        .controller('ReportViewerCtrl', ['$scope', '$rootScope', '$compile', 'ReportService', 'TestExecutionService', function ($scope, $rootScope, $compile, ReportService, TestExecutionService) {
             $scope.report = null;
             $scope.testStepId = null;
             $scope.error = null;
             $scope.loading = false;
-            $rootScope.$on($scope.type + ':reportLoaded', function (event, report,testStepId) {
+            $scope.testStepValidationReportId = null;
+            var destroyEvent3;
+            var destroyEvent2;
+            var destroyEvent1;
+            destroyEvent1 =  $rootScope.$on($scope.type + ':createMessageValidationReport', function (event, report, testStep) {
                 $scope.loading = true;
-                $scope.testStepId = testStepId;
-                if(report != null) {
-                    ReportService.save(report.xml, testStepId).then(function (response) {
+                $scope.testStepId = testStep.id;
+                if (report != null) {
+                    ReportService.createMessageValidationReport(report.xml, testStep.id).then(function (response) {
                         $scope.report = report;
                         $scope.report["id"] = response.id;
                         $scope.compile();
                         $scope.loading = false;
                     }, function (error) {
                         $scope.report = null;
+                        $scope.compile();
                         $scope.loading = false;
                         $scope.error = error;
                     });
-                }else{
+                } else {
                     $scope.report = null;
                     $scope.loading = false;
                 }
             });
 
+
+            destroyEvent2 = $rootScope.$on($scope.type + ':initValidationReport', function (event, report, testStep) {
+                $scope.loading = true;
+                $scope.testStepId = testStep.id;
+                if (report != null && report != undefined) {
+                    $scope.report = report;
+                    TestExecutionService.setTestStepValidationReportObject(testStep, $scope.report);
+                    $scope.compile();
+                    $scope.loading = false;
+                } else {
+                    $scope.report = null;
+                    $scope.compile();
+                    TestExecutionService.setTestStepValidationReportObject(testStep, null);
+                    $scope.loading = false;
+                    $scope.error = null;
+                }
+            });
+
+
+            destroyEvent3 = $rootScope.$on($scope.type + ':updateTestStepValidationReport', function (event, report, testStep) {
+                //$scope.loading = true;
+                $scope.testStepId = testStep.id;
+
+                var result = TestExecutionService.getTestStepValidationResult(testStep);
+                result= result != undefined ? result: null;
+                var comments = TestExecutionService.getTestStepComments(testStep);
+                comments = comments != undefined ? comments:null;
+                var xmlMessageOrManualValidation = report != null ? report.xml:null;
+                ReportService.updateTestStepValidationReport(xmlMessageOrManualValidation, testStep.id, result, comments).then(function (response) {
+                    $scope.report = response;
+                    TestExecutionService.setTestStepValidationReportObject(testStep, $scope.report);
+                    var back = TestExecutionService.getTestStepValidationReportObject(testStep);
+                    $scope.compile();
+//                    $scope.loading = false;
+                }, function (error) {
+                    $scope.report = null;
+//                    $scope.loading = false;
+                    $scope.compile();
+                    $scope.error = error;
+                });
+            });
+
+            $rootScope.$on('$destroy', function() {
+                destroyEvent1(); // remove listener.
+                destroyEvent2();
+                destroyEvent3();
+            });
+
+//
+//            $rootScope.$on($scope.type + ':updateTestStepValidationReport', function (event, report, testStep) {
+//                $scope.loading = true;
+//                $scope.testStepId = testStep.id;
+//                if (report != null) {
+//                    $scope.report = report;
+//                    $scope.compile();
+//                    $scope.loading = false;
+//                } else {
+//                    $scope.report = null;
+//                    $scope.compile();
+//                    $scope.loading = false;
+//                }
+//            });
+
+//            $rootScope.$on($scope.type + ':updateTestStepValidationReport', function (event, report, testStep) {
+//                $scope.loading = true;
+//                $scope.testStepId = testStep.id;
+//                if (report != null) {
+//                    $scope.report = report;
+//                    $scope.compile();
+//                    $scope.loading = false;
+//                } else {
+//                    $scope.report = null;
+//                    $scope.compile();
+//                    $scope.loading = false;
+//                }
+//            });
+
+
+
+
             $scope.compile = function () {
                 var element = $('#' + $scope.type + '-report');
-                if ($scope.report != null) {
+                if ($scope.report != null && $scope.report != undefined && $scope.report.html != null  && $scope.report.html != undefined) {
                     element.html($scope.report.html);
                 } else {
                     element.html('');
@@ -56,7 +141,11 @@
             };
 
             $scope.downloadAs = function (format) {
-                return ReportService.downloadTestStepReport( $scope.testStepId, format);
+                if ($scope.type === 'cb') {
+                    return ReportService.downloadTestStepValidationReport($scope.report.id, format);
+                } else {
+                    return ReportService.downloadMessageValidationReport($scope.report.id, format);
+                }
             };
         }]);
 
@@ -65,9 +154,9 @@
         var ReportService = function () {
         };
 
-        ReportService.downloadTestStepReport = function (testStepId, format) {
+        ReportService.downloadTestStepValidationReport = function (testStepValidationReportId, format) {
             var form = document.createElement("form");
-            form.action = "api/report/teststep/" + testStepId + "/download";
+            form.action = "api/testStepValidationReport/" + testStepValidationReportId + "/download";
             form.method = "POST";
             form.target = "_target";
             var input = document.createElement("input");
@@ -77,28 +166,65 @@
             form.style.display = 'none';
             document.body.appendChild(form);
             form.submit();
-
         };
 
-        ReportService.downloadTestCaseReports = function (testCaseId) {
-             var form = document.createElement("form");
-            form.action = "api/testcases/" + testCaseId + "/reports/download";
+        ReportService.downloadMessageValidationReport = function (testStepValidationReportId, format) {
+            var form = document.createElement("form");
+            form.action = "api/messageValidationReport/" + testStepValidationReportId + "/download";
             form.method = "POST";
             form.target = "_target";
+            var input = document.createElement("input");
+            input.name = "format";
+            input.value = format;
+            form.appendChild(input);
             form.style.display = 'none';
             document.body.appendChild(form);
             form.submit();
         };
 
-        ReportService.save = function (xmlReport, testStepId) {
+
+        ReportService.downloadTestCaseReports = function (testCaseId, format, result, comments) {
+            var form = document.createElement("form");
+            form.action = "api/testCaseValidationReport/download";
+            form.method = "POST";
+            form.target = "_target";
+
+            var input = document.createElement("input");
+            input.name = "format";
+            input.value = format;
+            form.appendChild(input);
+
+            input = document.createElement("input");
+            input.name = "testCaseId";
+            input.value = testCaseId;
+            form.appendChild(input);
+
+            input = document.createElement("input");
+            input.name = "result";
+            input.value = result;
+            form.appendChild(input);
+
+            input = document.createElement("input");
+            input.name = "comments";
+            input.value = comments;
+            form.appendChild(input);
+
+
+            form.style.display = 'none';
+            document.body.appendChild(form);
+            form.submit();
+        };
+
+
+        ReportService.createMessageValidationReport = function (xmlMessageValidationReport, testStepId) {
             var delay = $q.defer();
-            var data = $.param({xmlReport: xmlReport, testStepId: testStepId});
+            var data = $.param({xmlMessageValidationReport: xmlMessageValidationReport, testStepId: testStepId});
             var config = {
-                headers : {
+                headers: {
                     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
                 }
             };
-            $http.post("api/report/save", data,config).then(
+            $http.post("api/messageValidationReport/create", data, config).then(
                 function (object) {
                     delay.resolve(angular.fromJson(object.data));
                 },
@@ -110,28 +236,140 @@
         };
 
 
-        /**
-         * TODO: remove
-         * @param url
-         * @param json
-         * @returns {*}
-         */
-        ReportService.generate = function (content) {
+
+        ReportService.initTestStepValidationReport = function (testStepId) {
             var delay = $q.defer();
-            $http({
-                url: "api/report/generate",
-                data: $.param({'content': json}),
-                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-                method: 'POST',
-                timeout: 60000
-            }).success(function (data) {
-                delay.resolve(angular.fromJson(data));
-            }).error(function (err) {
-                delay.reject(err);
-            });
+            var data = $.param({testStepId: testStepId});
+            var config = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            };
+            $http.post("api/testStepValidationReport/init", data, config).then(
+                function (object) {
+                    delay.resolve(angular.fromJson(object.data));
+                },
+                function (response) {
+                    delay.reject(response.data);
+                }
+            );
+
+//            $http.get("../../resources/cb/initTestStep.json").then(
+//                function (object) {
+//                    delay.resolve(angular.fromJson(object.data));
+//                },
+//                function (response) {
+//                    delay.reject(response.data);
+//                }
+//            );
 
             return delay.promise;
         };
+
+
+
+        ReportService.updateTestStepValidationReport = function (xmlMessageValidationReport, testStepId, result, comments) {
+            var delay = $q.defer();
+            var data = $.param({xmlMessageValidationReport: xmlMessageValidationReport, testStepId: testStepId, result: result, comments: comments});
+            var config = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            };
+            $http.post("api/testStepValidationReport/update", data, config).then(
+                function (object) {
+                    delay.resolve(angular.fromJson(object.data));
+                },
+                function (response) {
+                    delay.reject(response.data);
+                }
+            );
+
+//            $http.get("../../resources/cb/updateTestStepReport.json").then(
+//                function (object) {
+//                    var report = angular.fromJson(object.data);
+//                    report['result'] = result;
+//                    report['comments'] = comments;
+//                    report['html'] =  "TestStep="+ testStepId + "<br/> Result=" + report['result'] + "<br /> Comments="+ report['comments'];
+//                    delay.resolve(report);
+//                },
+//                function (response) {
+//                    delay.reject(response.data);
+//                }
+//            );
+
+            return delay.promise;
+        };
+
+
+//        ReportService.updateTestStepValidationReport = function (testStep, result, comments) {
+//            var delay = $q.defer();
+//            var config = {
+//                headers: {
+//                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+//                }
+//            };
+//
+//            var data = $.param({
+//                result: result,
+//                comments: comments,
+//                testStepId: testStep.id
+//            });
+//            $http.post("api/testStepValidationReport/update", data, config).then(
+//                function (object) {
+//                    delay.resolve(angular.fromJson(object.data));
+//                },
+//                function (response) {
+//                    delay.reject(response.data);
+//                }
+//            );
+//            return delay.promise;
+//        };
+
+//        ReportService.generateHtml= function (testStepValidationReportId) {
+//            var delay = $q.defer();
+////            $http.post("api/testStepValidationReport/"+ testStepValidationReportId + "/generateHtml").then(
+////                function (object) {
+////                    delay.resolve(angular.fromJson(object.data));
+////                },
+////                function (response) {
+////                    delay.reject(response.data);
+////                }
+////            );
+//                $http.get("../../resources/cb/manualHTML.json").then(
+//                    function (object) {
+//                        delay.resolve(angular.fromJson(object.data));
+//                    },
+//                    function (response) {
+//                        delay.reject(response.data);
+//                    }
+//                );
+//
+//            return delay.promise;
+//        }
+
+
+//        /**
+//         * TODO: remove
+//         * @param json
+//         * @returns {*}
+//         */
+//        ReportService.generate = function (content) {
+//            var delay = $q.defer();
+//            $http({
+//                url: "api/testStepValidationReport/generate",
+//                data: $.param({'content': json}),
+//                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+//                method: 'POST',
+//                timeout: 60000
+//            }).success(function (data) {
+//                delay.resolve(angular.fromJson(data));
+//            }).error(function (err) {
+//                delay.reject(err);
+//            });
+//
+//            return delay.promise;
+//        };
 
         return ReportService;
     });
