60b54292e2f0002628ca5a35c70e3b34d9f609c8
Affo, Harold <affoharold@yahoo.fr>
2016-07-07 11:37:33 -0400

    dd

--- hit-base-web/client/app/lib/utils.js
+++ hit-base-web/client/app/lib/utils.js
@@ -71,7 +71,8 @@ mod.factory('MessageUtil',
                     } else {
                         //var hex = c.charCodeAt(0).toString(16);
 //                        var hex = ("0000" + c.charCodeAt(0).toString(4)).substr(-4);
-                        var hex = ("0000" +c.charCodeAt(0).toString(4));
+                        var hex = ("0000" + (+c.charCodeAt(0)).toString(16)).slice(-4);
+//                        var hex = ("0000" +c.charCodeAt(0).toString(4));
                         out += "[x" + hex + "]";
                     }
                 }
