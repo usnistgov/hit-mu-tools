0244945578811909bbb251ffa75dae42f0283e87
Affo, Harold <affoharold@yahoo.fr>
2018-05-11 11:22:27 -0400

    Implemented multitple domains and custom documentation

--- hit-base-web/client/app/scripts/main/service.js
+++ hit-base-web/client/app/scripts/main/service.js
@@ -8,34 +8,33 @@
  * Controller of the clientApp
  */
 angular.module('main')
-    .controller('MainService', function ($scope) {
-    });
+  .controller('MainService', function ($scope) {
+  });
 
 
 angular.module('main').factory('TestingSettings',
-    ['$rootScope', function ($rootScope) {
-        var service = {
-            activeTab:0,
-            getActiveTab: function(){
-                return service.activeTab;
-            },
-            setActiveTab: function(value){
-                service.activeTab = value;
-                service.save();
-            },
-            save: function () {
-                sessionStorage.TestingActiveTab = service.activeTab;
-            },
-            restore: function () {
-                service.activeTab = sessionStorage.TestingActiveTab != null && sessionStorage.TestingActiveTab != "" ? parseInt(sessionStorage.TestingActiveTab):0;
-            }
-        };
+  ['$rootScope', function ($rootScope) {
+    var service = {
+      activeTab:0,
+      getActiveTab: function(){
+        return service.activeTab;
+      },
+      setActiveTab: function(value){
+        service.activeTab = value;
+        service.save();
+      },
+      save: function () {
+        sessionStorage.TestingActiveTab = service.activeTab;
+      },
+      restore: function () {
+        service.activeTab = sessionStorage.TestingActiveTab != null && sessionStorage.TestingActiveTab != "" ? parseInt(sessionStorage.TestingActiveTab):0;
+      }
+    };
 //        $rootScope.$on("TestingSettings:save", service.save);
 //        $rootScope.$on("TestingSettings:restore", service.restore);
-        return service;
-    }]
+    return service;
+  }]
 );
-
 
 
 angular.module('main').service('modalService', ['$modal',	function ($modal) {
@@ -87,8 +86,6 @@ angular.module('main').service('modalService', ['$modal',	function ($modal) {
   };
 
 }]);
-
-
 
 
 
