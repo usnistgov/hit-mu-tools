5c62b3f6ff19a757cd2bb780cf6086886f3b7c1f
Harold Affo <affoharold@yahoo.fr>
2019-02-04 15:53:08 -0500

    Fixed domain list when not an admin

--- hit-base-web/client/app/scripts/domains/service.js
+++ hit-base-web/client/app/scripts/domains/service.js
@@ -14,6 +14,19 @@ angular.module('domains').factory('DomainsManager', ['$q', '$http',
                 return delay.promise;
             },
 
+            findByUserAndRole: function () {
+                var delay = $q.defer();
+                $http.get('api/domains/findByUserAndRole', {timeout: 60000}).then(
+                    function (object) {
+                        delay.resolve(angular.fromJson(object.data));
+                    },
+                    function (response) {
+                        delay.reject(response.data);
+                    }
+                );
+                return delay.promise;
+            },
+
             getDomains: function () {
                 var delay = $q.defer();
                 $http.get('api/domains', {timeout: 60000}).then(
