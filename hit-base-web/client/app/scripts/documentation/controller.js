4feabf5700952816cb9118b5901b58ab5999b3ea
Crouzier, Nicolas (Assoc) <>
2019-11-20 13:45:56 -0500

    Added missing link check in installation guide and tool download document page

--- hit-base-web/client/app/scripts/documentation/controller.js
+++ hit-base-web/client/app/scripts/documentation/controller.js
@@ -1104,6 +1104,26 @@ angular.module('doc')
       $scope.initDocs(scope, 500);
     });
 
+    $scope.isLink = function(path) {
+    	return path && path != null && path.startsWith("http");
+	};
+	
+	$scope.downloadDocument = function(path) {
+		if (path != null) {
+			var form = document.createElement("form");
+			form.action = "api/documentation/downloadDocument";
+			form.method = "POST";
+			form.target = "_target";
+			var input = document.createElement("input");
+			input.name = "path";
+			input.value = path;
+			form.appendChild(input);
+			form.style.display = 'none';
+			document.body.appendChild(form);
+			form.submit();
+		}
+	};
+
 
     $scope.addDocument = function () {
       $scope.actionError = null;
@@ -1320,21 +1340,25 @@ angular.module('doc')
     });
 
 
-    $scope.downloadDocument = function (path) {
-      if (path != null) {
-        var form = document.createElement("form");
-        form.action = "api/documentation/downloadDocument";
-        form.method = "POST";
-        form.target = "_target";
-        var input = document.createElement("input");
-        input.name = "path";
-        input.value = path;
-        form.appendChild(input);
-        form.style.display = 'none';
-        document.body.appendChild(form);
-        form.submit();
-      }
-    };
+    $scope.isLink = function(path) {
+    	return path && path != null && path.startsWith("http");
+	};
+	
+	$scope.downloadDocument = function(path) {
+		if (path != null) {
+			var form = document.createElement("form");
+			form.action = "api/documentation/downloadDocument";
+			form.method = "POST";
+			form.target = "_target";
+			var input = document.createElement("input");
+			input.name = "path";
+			input.value = path;
+			form.appendChild(input);
+			form.style.display = 'none';
+			document.body.appendChild(form);
+			form.submit();
+		}
+	};
 
 
     $scope.addDocument = function () {
